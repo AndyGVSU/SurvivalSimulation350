7f3e2be2e7c00cc0f6f737b3eaec6025fa7c8d19
Skarzynp <35232423+Skarzynp@users.noreply.github.com>
2018-10-02 19:18:20 -0400

    Add files via upload

---
+++ Plant.java
@@ -0,0 +1,15 @@
+package simulation;
+
+public class Plant extends Entity {
+	
+	
+	public Plant(String name,int nutrients, int height, int width) {
+		super(name,nutrients,height,width);
+		
+	}
+	
+	
+	
+	
+
+}
