126d58fb5d262dfc29d065fca1b5722d4ec5cba6
Brendon Murthum <bmmurthum@gmail.com>
2018-11-20 03:34:52 -0500

    Has functional reproduction. Plant death seems glitched at times.

--- simulation/Fruit.java
+++ simulation/Fruit.java
@@ -1,5 +1,7 @@
 package simulation;
 
+import java.util.Random;
+
 /**
  *  The fruit entity is created by a "stem-plant entity". It is created in the
  *  place of a recently removed leaf. This fruit is intended to act as a seed.
@@ -32,7 +34,7 @@ public class Fruit extends Entity {
      * The number of spaces to the side that the fruit will try to root into
      * when it falls off the plant.
      */
-    private final int fruitDropSideDistance = 2;
+    private final int fruitDropSideDistance = 3;
 
     /**
      * @return  The number of ticks that this particular fruit takes to drop a
@@ -64,18 +66,52 @@ public class Fruit extends Entity {
         //   Starts at the right and searches to the left.
         boolean successfullyPlanted = false;
 
-        int tempCol;
-        for (tempCol = (col + fruitDropSideDistance); Math.abs(tempCol - col) < fruitDropSideDistance && !successfullyPlanted; tempCol--) {
-            System.out.print(tempCol + " ");
+        // Put columns to check into a random order
+        int lowestColumn;
+        int highestColumn;
+        if (col - fruitDropSideDistance < 0) {
+            lowestColumn = 0;
+        } else {
+            lowestColumn = col - fruitDropSideDistance;
+        }
+        if (col + fruitDropSideDistance > simulation.getColumns()) {
+            highestColumn = simulation.getColumns();
+        } else {
+            highestColumn = col + fruitDropSideDistance;
+        }
+        int numberOfColumns = highestColumn - lowestColumn + 1;
+        int[] columnsToCheck = new int[numberOfColumns];
+        Random randomSeed = new Random();
+        // Add to each index
+        for (int i = 0; i < numberOfColumns - 1; i++) {
+            boolean addedEntry = false;
+            // Is this new value worth adding?
+            while (!addedEntry) {
+                int randomInt = randomSeed.nextInt(numberOfColumns) + lowestColumn;
+                boolean isInArray = false;
+                for (int j = 0; j < numberOfColumns; j++) {
+                    if (columnsToCheck[j] == randomInt) {
+                        isInArray = true;
+                    }
+                }
+                if (!isInArray) {
+                    columnsToCheck[i] = randomInt;
+                    addedEntry = true;
+                }
+            }
+        }
 
+        // Check each of these random columns-within-range for valid
+        // drop spots.
+        int tempCol;
+        for (int i = 0; i <  numberOfColumns && !successfullyPlanted; i++) {
+            // A random column within range to check for valid planting
+            tempCol = columnsToCheck[i];
             // Iterate "downward" until hitting dirt.
             for (int tempRow = row; tempRow < simulation.getRows();
                  tempRow++) {
-                System.out.print(tempRow + " ");
                 // Found dirt!
                 if (simulation.getEntity(tempRow, tempCol).getSymbol() == 'D') {
-                    System.out.println("  Dirt at: r" + tempRow + " c" + tempCol);
-
                     // If the spot above the dirt is air..
                     if (simulation.getEntity(tempRow - 1,
                             tempCol).getSymbol() == '.') {
@@ -84,7 +120,6 @@ public class Fruit extends Entity {
                                         tempRow - 1, tempCol));
                         // This allows for breaking from the parent loop.
                         successfullyPlanted = true;
-                        System.out.println("TriedToPlant at r" + (tempRow - 1) + " - c" + tempCol);
                     }
                     // Break from the downward search when hitting dirt.
                     break;
