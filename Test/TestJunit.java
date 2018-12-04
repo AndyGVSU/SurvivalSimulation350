35ab5bfd256d5f68bd853308df9fee4d085044a9
Skarzynp <35232423+Skarzynp@users.noreply.github.com>
2018-12-04 15:51:44 -0500

    Add files via upload

---
+++ Test/TestJunit.java
@@ -0,0 +1,129 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import simulation.*;
+
+import java.awt.*;
+import java.util.Objects;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+
+public class TestJunit {
+
+    private MainSimulation Testsimulation;
+    private int row = 32;
+    private int col = 32;
+
+    @Before
+    public void setUp() {
+
+        this.Testsimulation = new MainSimulation(row, col);
+
+    }
+
+    @Test
+    public void Test1CheckRows() {
+
+        assertEquals(32, Testsimulation.getRows());
+    }
+
+    @Test
+    public void Test2CheckCols() {
+
+        assertEquals(32, Testsimulation.getColumns());
+    }
+
+
+    @Test
+    public void Test3CheckSpeed() {
+        boolean ex = false;
+        this.Testsimulation.setSpeed(10);
+        if (this.Testsimulation.getSpeed() == 10) {
+            ex = true;
+        }
+        assertTrue(ex);
+
+    }
+
+    @Test
+    public void Test4setEntity() {
+        this.Testsimulation.setEntity(99, 200, new Dirt(this.Testsimulation, null, 0, 99, 200));
+    }
+
+    @Test
+    public void NutrientTest1() {
+
+        this.Testsimulation.getEntity(3, 3).setNutrients(90000);
+    }
+
+    @Test
+    public void NutrientTest2() {
+
+        this.Testsimulation.getEntity(3, 3).setNutrients(-5000);
+    }
+
+    @Test
+    public void NutrientTest3() {
+
+        this.Testsimulation.getEntity(3, 3).setNutrients(12);
+        assertEquals(12, this.Testsimulation.getEntity(3, 3).getNutrients());
+
+    }
+
+    @Test
+    public void TestCanLive() {
+        this.Testsimulation.getEntity(3, 3).setNutrients(12);
+        if (this.Testsimulation.getEntity(3, 3).getNutrients() > 0) {
+            assertTrue(this.Testsimulation.getEntity(3, 3).canLive());
+        }
+    }
+
+    @Test
+    public void TestGetColor() {
+        for (int i = 0; i < this.Testsimulation.getRows(); i++) {
+            for (int j = 0; j < this.Testsimulation.getColumns(); j++) {
+                if (this.Testsimulation.getEntity(i, j) instanceof Dirt) {
+                    assertEquals(2, this.Testsimulation.getEntity(i, j).getColor());
+                }
+                if (this.Testsimulation.getEntity(i, j) instanceof Air) {
+                    assertEquals(1, this.Testsimulation.getEntity(i, j).getColor());
+                }
+                if (this.Testsimulation.getEntity(i, j) instanceof Grass) {
+                    assertEquals(0, this.Testsimulation.getEntity(i, j).getColor());
+                }
+                if (this.Testsimulation.getEntity(i, j) instanceof Leaf) {
+                    assertEquals(3, this.Testsimulation.getEntity(i, j).getColor());
+                }
+                if (this.Testsimulation.getEntity(i, j) instanceof Fruit) {
+                    assertEquals(5, this.Testsimulation.getEntity(i, j).getColor());
+                }
+            }
+        }
+    }
+    @After
+    public void ResetTest() {
+        this.Testsimulation.reset(true);
+
+            for (int j = 0; j < this.Testsimulation.getColumns(); j++) {
+                assertEquals('.', this.Testsimulation.getEntity(25, j).getSymbol());
+            }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
